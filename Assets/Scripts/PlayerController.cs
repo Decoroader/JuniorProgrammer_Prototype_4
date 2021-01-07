c034caa1befe8a9a5056a7351ee21293e0c80e1a
Decoroader <secbittree@gmail.com>
2021-01-07 22:03:49 +0200

    Lesson 4.1 - Watch Where You’re Going, 6
    6.Add forward force to the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speedPlayer = 1.5f;
+    private Rigidbody playerRigid;
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float playerMoveF_B = Input.GetAxis("Vertical");
+        playerRigid.AddForce(Vector3.forward * playerMoveF_B * speedPlayer, ForceMode.Impulse);
+    }
+}
