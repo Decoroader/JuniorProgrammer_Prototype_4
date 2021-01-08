05100b19ee43a993418116ccdd8a55e9a3308eaa
Decoroader <secbittree@gmail.com>
2021-01-08 10:51:59 +0200

    Lesson 4.2 - Follow the Player, 5, 6, 7
    7th step was not passed due no need - the rand generation of a position into the Instantiate() method

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private Vector3 enemyPos;
+    private float range = 9;
+    void Start()
+    {
+        enemyPos = new Vector3(Random.Range(-range, range), 0.5f, Random.Range(-range, range));
+        Instantiate(enemyPrefab, enemyPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
