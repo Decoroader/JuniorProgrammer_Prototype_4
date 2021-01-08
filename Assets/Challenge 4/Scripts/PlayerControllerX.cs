1dbcb767019779d4adb4fb68d11b1cc8d79c9a0a
Decoroader <secbittree@gmail.com>
2021-01-08 23:05:00 +0200

    Challenge 4 - Soccer Scripting
    7.Bonus: The player needs a turbo boost

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -6,14 +6,17 @@ public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
     private float speed = 500;
+    private float boostSpeed = 1;
     private GameObject focalPoint;
 
     public bool hasPowerup;
     public GameObject powerupIndicator;
     public int powerUpDuration = 5;
+    public ParticleSystem boostEffect;
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+    private Vector3 pearticleOffset = new Vector3(0, 0, -0.55f);
     
     void Start()
     {
@@ -23,9 +26,18 @@ public class PlayerControllerX : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            boostSpeed = 3;
+            boostEffect.transform.position = transform.position - pearticleOffset;
+            boostEffect.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+            boostSpeed = 1;
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput 
+            * speed * boostSpeed * Time.deltaTime); 
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
